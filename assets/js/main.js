d1574e9eda63dd80e600805dada0837228fa2533
Tom <tom.goyens@gmail.com>
2019-03-22 11:38:18 +0100

    voorlopige live versie

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
-var englishBtn = document.getElementById("english");
-var nederlandsBtn = document.getElementById("nederlands");
+var englishBtn = document.getElementById("en");
+var nederlandsBtn = document.getElementById("nl");
 const hobbiesTitle = document.querySelector(".hobbiesTitle");
 const hobbiesList = document.querySelector(".hobbiesList");
+const profielTitle = document.querySelector(".profielTitle");
+const profielText = document.querySelector(".profielText");
 const eduTitle = document.querySelector(".eduTitle");
 const eduList = document.querySelector(".eduList");
 const expTitle = document.querySelector(".expTitle");
@@ -28,6 +30,7 @@ var xhr2 = new XMLHttpRequest();
 xhr2.onload = function() {
   if (this.status == 200) {
       nl = JSON.parse(this.responseText);
+      // translateToNederlands();
   }
 }
 
@@ -37,124 +40,74 @@ xhr2.send();
 englishBtn.addEventListener("click", translateToEnglish);
 
 function translateToEnglish(){
-
-  hobbiesTitle.innerHTML = en.hobbies.title+":";
-  hobbiesList.innerHTML = "";
-  let temp;
-  let temp2;
-  let temp3;
-  for(let i = 0; i < en.hobbies.list.length; i++){
-    temp = document.createElement('li');
-    temp.innerHTML = en.hobbies.list[i];
-    hobbiesList.appendChild(temp);
-  }
-
-  eduTitle.innerHTML = en.education.title+":";
-  eduList.innerHTML = "";
-  for(let i = 0; i < en.education.list.length; i++){
-    temp = document.createElement('div');
-    temp.classList.add("container");
-    temp2 = document.createElement('p');
-    temp2.innerHTML = en.education.list[i][0];
-    temp.appendChild(temp2);
-    temp2 = document.createElement('p');
-    temp2.classList.add("tabbed");
-    temp2.innerHTML = "<strong>"+en.education.list[i][1]+":</strong> </br> "+en.education.list[i][2];
-    temp.appendChild(temp2);
-    eduList.appendChild(temp);
-  }
-
-  expTitle.innerHTML = en.experience.title+":";
-  expList.innerHTML = "";
-  for(let i = 0; i < en.experience.list.length; i++){
-    temp = document.createElement('div');
-    temp.classList.add("container");
-    temp2 = document.createElement('p');
-    temp2.innerHTML = en.experience.list[i][0];
-    temp.appendChild(temp2);
-    temp2 = document.createElement('p');
-    temp2.classList.add("tabbed");
-    temp2.innerHTML = "<strong>"+en.experience.list[i][1]+":</strong> </br> "+en.experience.list[i][2];
-    temp.appendChild(temp2);
-    expList.appendChild(temp);
-  }
-
-  skillsTitle.innerHTML = en.skills.title+":";
-  skillsList.innerHTML = "";
-  for(let i = 0; i < en.skills.list.length; i++){
-    temp = document.createElement('li');
-    if(en.skills.list[i].length == 1){
-      temp.innerHTML = en.skills.list[i][0];
-    } else {
-      temp.innerHTML = en.skills.list[i][0]+":";
-      temp2 = document.createElement('ul');
-      for(let j = 0; j < en.skills.list[i][1].length; j++){
-        temp3 = document.createElement('li');
-        temp3.innerHTML = en.skills.list[i][1][j];
-        temp2.appendChild(temp3);
-      }
-      temp.appendChild(temp2);
-    }
-    skillsList.appendChild(temp);
-  }
+ translate(en);
 }
 nederlandsBtn.addEventListener("click", translateToNederlands);
 
 function translateToNederlands(){
+ translate(nl);
+}
 
-  hobbiesTitle.innerHTML = nl.hobbies.title+":";
-  hobbiesList.innerHTML = "";
+
+function translate(lang){
   let temp;
   let temp2;
   let temp3;
-  for(let i = 0; i < nl.hobbies.list.length; i++){
+
+  hobbiesTitle.innerHTML = lang.hobbies.title+":";
+  hobbiesList.innerHTML = "";
+  for(let i = 0; i < lang.hobbies.list.length; i++){
     temp = document.createElement('li');
-    temp.innerHTML = nl.hobbies.list[i];
+    temp.innerHTML = lang.hobbies.list[i];
     hobbiesList.appendChild(temp);
   }
 
-  eduTitle.innerHTML = nl.education.title+":";
+  profielTitle.innerHTML = lang.profiel.title+":";
+  profielText.innerHTML = lang.profiel.profielText;
+
+  eduTitle.innerHTML = lang.education.title+":";
   eduList.innerHTML = "";
-  for(let i = 0; i < nl.education.list.length; i++){
+  for(let i = 0; i < lang.education.list.length; i++){
     temp = document.createElement('div');
     temp.classList.add("container");
     temp2 = document.createElement('p');
-    temp2.innerHTML = nl.education.list[i][0];
+    temp2.innerHTML = lang.education.list[i][0];
     temp.appendChild(temp2);
     temp2 = document.createElement('p');
     temp2.classList.add("tabbed");
-    temp2.innerHTML = "<strong>"+nl.education.list[i][1]+":</strong> </br> "+nl.education.list[i][2];
+    temp2.innerHTML = "<strong>"+lang.education.list[i][1]+"</strong> </br> "+lang.education.list[i][2] + "<br>"+ lang.education.list[i][3];
     temp.appendChild(temp2);
     eduList.appendChild(temp);
   }
 
-  expTitle.innerHTML = nl.experience.title+":";
+  expTitle.innerHTML = lang.experience.title+":";
   expList.innerHTML = "";
-  for(let i = 0; i < nl.experience.list.length; i++){
+  for(let i = 0; i < lang.experience.list.length; i++){
     temp = document.createElement('div');
     temp.classList.add("container");
     temp2 = document.createElement('p');
-    temp2.innerHTML = nl.experience.list[i][0];
+    temp2.innerHTML = lang.experience.list[i][0];
     temp.appendChild(temp2);
     temp2 = document.createElement('p');
     temp2.classList.add("tabbed");
-    temp2.innerHTML = "<strong>"+nl.experience.list[i][1]+":</strong> </br> "+nl.experience.list[i][2];
+    temp2.innerHTML = "<strong>"+lang.experience.list[i][1]+"</strong> </br> "+lang.experience.list[i][2] + "<br>"+ lang.education.list[i][3];
     temp.appendChild(temp2);
     expList.appendChild(temp);
   }
 
-  skillsTitle.innerHTML = nl.skills.title+":";
+  skillsTitle.innerHTML = lang.skills.title+":";
   skillsList.innerHTML = "";
-  for(let i = 0; i < nl.skills.list.length; i++){
+  for(let i = 0; i < lang.skills.list.length; i++){
     temp = document.createElement('li');
-    if(nl.skills.list[i].length == 1){
-      temp.innerHTML = nl.skills.list[i][0];
+    if(en.skills.list[i].length == 1){
+      temp.innerHTML = lang.skills.list[i][0];
     } else {
-      temp.innerHTML = nl.skills.list[i][0]+":";
+      temp.innerHTML = lang.skills.list[i][0]+":";
       temp2 = document.createElement('ul');
-      for(let j = 0; j < nl.skills.list[i][1].length; j++){
+      temp2.classList.add("listStyle1");
+      for(let j = 0; j < lang.skills.list[i][1].length; j++){
         temp3 = document.createElement('li');
-        temp3.innerHTML = nl.skills.list[i][1][j];
+        temp3.innerHTML = lang.skills.list[i][1][j];
         temp2.appendChild(temp3);
       }
       temp.appendChild(temp2);
